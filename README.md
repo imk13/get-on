14c9a9781b97024efae898d054ae03915320b52c
Mukesh Kumar <kumar.mukesh5771884@gmail.com>
2019-10-17 02:28:42 +0530

    get-on changes: swagger doc added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### get-on
+# get-on
 Local setup
 * make sure node version is > 8.x. To install node version nvm `nvm install 8.x`
 * install mongodb | follow this link  https://www.digitalocean.com/community/tutorials/how-to-install-mongodb-on-ubuntu-18-04
@@ -15,6 +15,7 @@ Prod setup
 * `npm run app:build`  to build node app image in prod env
 * `npm run app:prod` to run node app in prod env 
 
-#App will listen on http://localhost:9090/
+### App will listen on http://localhost:9090/
+### Swagger doc link http://localhost:9090/api-docs/
 
 * NOTE: https://stackoverflow.com/questions/48957195/how-to-fix-docker-got-permission-denied-issue 
--- src/api/routes/v1/question.route.js
+++ src/api/routes/v1/question.route.js
@@ -10,23 +10,160 @@ const {
 
 const router = express.Router();
 
+/**
+ * @swagger
+ *
+ * definitions:
+ *   Question:
+ *     type: object
+ *     required:
+ *       - question
+ *     properties:
+ *       question:
+ *         type: string
+ *       metadata:
+ *         type: object
+ *         description: Holds metadata for the question such as for video,images, MCQ options etc
+ *       is_active:
+ *         type: boolean
+ *         default: true
+ *       created_by:
+ *         type: string
+ *       created_at: 
+ *         type: string
+ *         example: "2019-10-16T07:56:45.019Z"
+ *         format: date-time
+ *       updated_at:
+ *         type: string
+ *         example: "2019-10-16T07:56:45.019Z"
+ *         format: date-time
+ */
+
 router
   .route('/')
-  /**
-   * get list of question
-   */
+/**
+ * @swagger
+ * /question:
+ *   get:
+ *     description: Returns list quetions
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: page
+ *         in: query
+ *         schema:
+ *           type: number
+ *         required:
+ *         - page
+ *       - name: limit
+ *         in: query
+ *         schema:
+ *           type: number
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: Question result
+ *         schema:
+ *           type: array
+ *           items:
+ *             $ref: '#/definitions/Question'
+ */
   .get(validate(list), controller.list)
-  /**
-   * create question
-   */
+/**
+ * @swagger
+ * /question:
+ *   post:
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         required:
+ *           - question
+ *         properties:
+ *           question:
+ *             type: string
+ *           metadata:
+ *             type: object
+ *             description: Holds metadata for the question such as for video,images, MCQ options etc
+ *           is_active:
+ *             type: boolean
+ *             default: true
+ *           created_by:
+ *             type: string
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: create question result
+ *         schema:
+ *           type: object
+ *           items:
+ *             $ref: '#/definitions/Question'
+ */
   .post(validate(create), controller.create)
-  /**
-   * update question
-   */
+/**
+ * @swagger
+ * /question:
+ *   put:
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         required:
+ *           - _id
+ *         properties:
+ *           _id:
+ *             type: string
+ *             format: uuid
+ *           question:
+ *             type: string
+ *           metadata:
+ *             type: object
+ *             description: Holds metadata for the question such as for video,images, MCQ options etc
+ *           is_active:
+ *             type: boolean
+ *             default: true
+ *           created_by:
+ *             type: string
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: create question result
+ *         schema:
+ *           type: object
+ *           items:
+ *             $ref: '#/definitions/Question'
+ */
   .put(validate(remove), controller.update)
-  /**
-   * delete question
-   */
+/**
+ * @swagger
+ * /question:
+ *   delete:
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         required:
+ *           - _id
+ *         properties:
+ *           _id:
+ *             type: string
+ *             format: uuid
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: create question result
+ *         schema:
+ *           type: object
+ *           items:
+ *             $ref: '#/definitions/Question'
+ */
   .delete(validate(remove), controller.delete)
 
 
